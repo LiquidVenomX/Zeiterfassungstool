9b58e6b705e43bd69d9a8f3ebc9762ea2ae6bd33
LiquidVenomX <155914774+LiquidVenomX@users.noreply.github.com>
2024-01-16 18:39:28 +0100

    adding functionalities & bug fixing
    dashboard added and updated credentials for testing

--- Zeiterfassung_flask_httpd.py
+++ Zeiterfassung_flask_httpd.py
@@ -1,19 +1,17 @@
-from flask import Flask, render_template, redirect, url_for, request
-from flask_login import LoginManager, UserMixin, login_required, login_user, current_user
 import sqlite3
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, UserMixin, login_required, login_user, current_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'geheimeschluessel'
 login_manager = LoginManager(app)
 
-# Benutzerklasse für Flask-Login
 class User(UserMixin):
     def __init__(self, user_id, username, password):
         self.id = user_id
         self.username = username
         self.password = password
 
-# Login-Manager-Konfiguration
 @login_manager.user_loader
 def load_user(user_id):
     conn = sqlite3.connect('user_database.db')
@@ -25,7 +23,31 @@ def load_user(user_id):
         return User(user_data[0], user_data[1], user_data[2])
     return None
 
-# Routen für Flask-Webanwendung
+def create_user():
+    conn = sqlite3.connect('user_database.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', ('admin', '1234'))
+    conn.commit()
+    conn.close()
+
+conn = sqlite3.connect('user_database.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT,
+                password TEXT)''')
+conn.commit()
+conn.close()
+
+conn = sqlite3.connect('user_database.db')
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM users WHERE username = ?', ('admin',))
+existing_user = cursor.fetchone()
+conn.close()
+
+if not existing_user:
+    create_user()
+
 @app.route('/')
 def index():
     return render_template('login.html')
@@ -47,6 +69,7 @@ def login():
             login_user(user)
             print(f"Benutzer {username} erfolgreich angemeldet!")
             return redirect(url_for('dashboard'))
+
         else:
             print("Falscher Benutzername oder Passwort!")
 
@@ -55,7 +78,13 @@ def login():
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return f'Dashboard für Benutzer: {current_user.username}'
+    return render_template('dashboard.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
